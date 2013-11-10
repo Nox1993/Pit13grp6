2adffcb7c34d165ec1e49922aef99ce28665ceea
Nox1993 <jan_kost_1993@yahoo.de>
2013-11-11 00:22:24 +0100

    11.11.2013/00:22 added Faktoren.cpp

---
+++ Project10/Faktoren.cpp
@@ -0,0 +1,28 @@
+#include "Faktoren.h"
+
+using namespace std; 
+
+Faktoren::Faktoren(){
+	Faktoren::spannung = 0;
+	Faktoren::temperatur = 0;
+	Faktoren::prozess = 0;
+	Faktoren::spannungFaktor = 0;
+	Faktoren::temperaturFaktor = 0;
+	Faktoren::prozessFaktor = 0;
+}
+
+Faktoren::Faktoren(){
+
+}
+
+void ausgabeFaktoren(){
+
+}
+
+void getFaktoren(double& spgFaktor, double& tmpFaktor, double& przFaktor){
+
+}
+
+
+
+
