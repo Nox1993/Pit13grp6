cff2487afb0181daeb10ee6251a2030ce5a182c7
youruser <nitnatsnok.s@gmail.com>
2013-11-12 17:13:13 +0100

    Create SchaltwerkElement.cpp

---
+++ Project10/SchaltwerkElement.cpp
@@ -0,0 +1,11 @@
+#include "SchaltwerkElement.h"
+
+
+SchaltwerkElement::SchaltwerkElement( GatterTyp* gTyp ){
+    //alle Attribute aus NULL für Zeiger initialisieren
+    //Auÿerdem bekommt der Konstruktor einen Zeiger auf ein Element der Bibliotheksdatenbank und speichert es in das Attribut typ.
+
+~SchaltwerkElement::SchaltwerkElement(){
+    }
+
+}
