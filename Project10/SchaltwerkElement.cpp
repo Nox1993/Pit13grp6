9ad1c047b62a1db0d0360c5429d6dbd68dd6e93c
youruser <nitnatsnok.s@gmail.com>
2013-11-17 14:41:28 +0100

    Update SchaltwerkElement.cpp

--- Project10/SchaltwerkElement.cpp
+++ Project10/SchaltwerkElement.cpp
@@ -5,7 +5,23 @@ SchaltwerkElement::SchaltwerkElement( GatterTyp* gTyp ){
     //alle Attribute aus NULL für Zeiger initialisieren
     //Auÿerdem bekommt der Konstruktor einen Zeiger auf ein Element der Bibliotheksdatenbank und speichert es in das Attribut typ.
 
+SchaltwerkElement::SchaltwerkElement(GatterTyp* gTyp){
+    //alle Attribute aus NULL für Zeiger initialisieren
+    //Auÿerdem bekommt der Konstruktor einen Zeiger auf ein Element der Bibliotheksdatenbank und speichert es in das Attribut typ.
+    name = null;
+    typ = null;
+    laufzeitEinzelgatter = 0;
+    nachfolgerElemente = null;
+    anzahlNachfolger = 0;
+    isEingangsElement = null;
+    isAusgangsElement = null;
+    anzahlEingangssignale = null;
+    
+}
 ~SchaltwerkElement::SchaltwerkElement(){
-    }
 
+}
+
+SchaltwerkElement::getLaufzeitEinzelgatter(){
+    return laufzeitEinzelgatter;
 }
