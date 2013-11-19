7fd71188455037c7235df95535d9c856e3f5b417
youruser <nitnatsnok.s@gmail.com>
2013-11-19 18:17:55 +0100

    Update SchaltwerkElement.cpp

--- Project10/SchaltwerkElement.cpp
+++ Project10/SchaltwerkElement.cpp
@@ -1,9 +1,8 @@
 #include "SchaltwerkElement.h"
 
-
 SchaltwerkElement::SchaltwerkElement( GatterTyp* gTyp ){
     //alle Attribute aus NULL für Zeiger initialisieren
-    name = NULL;
+    name = "";
     //Ausserdem bekommt der Konstruktor einen Zeiger auf ein Element der Bibliotheksdatenbank und speichert es in das Attribut typ.
     Bibliothek::bibElemente->typ;
     laufzeitEinzelgatter = 0;
@@ -65,29 +64,29 @@ void SchaltwerkElement::setLaufzeitEinzelgatter(double lfz){
 }
 
 /*
-public:
-    void SchaltwerkElement::getName();
-    void SchaltwerkElement::getTyp();
-    double SchaltwerkElement::getLaufzeitEinzelgatter();
-    SchaltwerkElement::SchaltwerkElement getNachfolger(int pos);
-    int SchaltwerkElement::getAnzahlEingangssignale();
-    bool SchaltwerkElement::getIsEingangsElement();
-    bool SchaltwerkElement::getIsAusgangsElement();
-    void SchaltwerkElement::setname(string n);
-    void SchaltwerkElement::nachfolgerHinzufuegen(SchaltwerkElement schaltwerkElement, int pos);
-    void SchaltwerkElement::setAnzahlNachfolger(int Anzahl);
-    void SchaltwerkElement::setAnzahlEingangssignale(short anzahl);
-    void SchaltwerkElement::setEingangsElement(bool isEingangsEl);
-    void SchaltwerkElement::setisAusgangsElement(bool isAusangsEl);
-    void SchaltwerkElement::setLaufzeitEinzelgatter(double lfz);
-    
-private:
-    string SchaltwerkElement::name;
-    Gattertyp* SchaltwerkElement::typ;
-    double SchaltwerkElement::laufzeitEinzelgatter;
-    SchaltwerkElement* SchaltwerkElement::nachfolgerElemente;
-    int SchaltwerkElement::anzahlNachfolger;
-    bool SchaltwerkElement::isEingangsElement;
-    bool SchaltwerkElement::isAusgangsElement;
-    short SchaltwerkElement::anzahlEingangssignale;
-*/
+ public:
+ void SchaltwerkElement::getName();
+ void SchaltwerkElement::getTyp();
+ double SchaltwerkElement::getLaufzeitEinzelgatter();
+ SchaltwerkElement::SchaltwerkElement getNachfolger(int pos);
+ int SchaltwerkElement::getAnzahlEingangssignale();
+ bool SchaltwerkElement::getIsEingangsElement();
+ bool SchaltwerkElement::getIsAusgangsElement();
+ void SchaltwerkElement::setname(string n);
+ void SchaltwerkElement::nachfolgerHinzufuegen(SchaltwerkElement schaltwerkElement, int pos);
+ void SchaltwerkElement::setAnzahlNachfolger(int Anzahl);
+ void SchaltwerkElement::setAnzahlEingangssignale(short anzahl);
+ void SchaltwerkElement::setEingangsElement(bool isEingangsEl);
+ void SchaltwerkElement::setisAusgangsElement(bool isAusangsEl);
+ void SchaltwerkElement::setLaufzeitEinzelgatter(double lfz);
+ 
+ private:
+ string SchaltwerkElement::name;
+ Gattertyp* SchaltwerkElement::typ;
+ double SchaltwerkElement::laufzeitEinzelgatter;
+ SchaltwerkElement* SchaltwerkElement::nachfolgerElemente;
+ int SchaltwerkElement::anzahlNachfolger;
+ bool SchaltwerkElement::isEingangsElement;
+ bool SchaltwerkElement::isAusgangsElement;
+ short SchaltwerkElement::anzahlEingangssignale;
+ */
