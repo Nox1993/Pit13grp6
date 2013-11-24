b0002e22c8c1b51e6bb72a6b3e391f0a07db85a7
SabrinaKost <SabrinaKost@users.noreply.github.com>
2013-11-24 18:37:53 +0100

    Header Faktoren 24.11.

---
+++ Project10/FaktorenS.h
@@ -0,0 +1,32 @@
+#pragma once
+class Faktoren
+{
+public:
+	Faktoren(void);
+	~Faktoren(void);
+
+private:
+	double spannung;
+	double temperatur;
+	double prozess;
+	double spannungFaktor;
+	double temperaturFaktor;
+	double prozessFaktor;
+
+	bool berechneSpannungFaktor(double spg);
+	bool berechneTemperaturFaktor (double temp);
+	bool berechneProzessFaktor (short prz);
+	double berechneFaktor (double value, double arr[][2], int laenge);
+	double interpolation (double value, double x1, double x2, double y1, double y2);
+
+	double getSpannung();
+	double getTemperatur();
+	short getProzess();
+	void getFaktoren(double& spgFaktor, double& tmpFaktor, double& przFaktor);
+	bool setSpannung (double spannung);
+	bool setTemperatur(double temperatur);
+	bool setProzess (short prozess);
+	void ausgabeFaktoren();
+
+};
+
