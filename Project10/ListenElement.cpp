6fb1c09451ac156c45483b1a029302da6157f64e
youruser <nitnatsnok.s@gmail.com>
2013-11-25 16:49:57 +0100

    Update ListenElement.cpp

--- Project10/ListenElement.cpp
+++ Project10/ListenElement.cpp
@@ -1,54 +1,33 @@
+//
+//  ListenElement.cpp
+//  Praktikum Informationstechnik
+//
+//  Created by Konstantin Sieler on 19.11.13.
+//  Copyright (c) 2013 Konstantin Sieler. All rights reserved.
+//
+
 #include "ListenElement.h"
 
 ListenElement::ListenElement(){
-  schaltwerkElement = NULL;
-  next = NULL;
-  //ALLE ZEIGER_ATTRIBUTE MIT NULL INITIALISIEREN
+    schaltwerkElement = NULL;
+    next = NULL;
+    //ALLE ZEIGER_ATTRIBUTE MIT NULL INITIALISIEREN
 }
 ~ListenELement::ListenElement(){
 }
 
 ListenElement::SchaltwerkElement* getSchaltwerkElement(){
-  return schaltwerkElement;
+    return schaltwerkElement;
 }
 
 ListenElement::ListenElement* getNextElement(){
-  return next;
+    return next;
 }
 
 ListenElement::void setSchaltwerkElement(){
-
+    
 }
 
 ListenElement::void setNextElement(){
-
+    
 }
-
-
-
-
-
-/*
--------HEADER------------------
-
-class ListenElement {
-
-public:
-  
-  ListenElement();
-  ~ListenElement();
-  
-  SchaltwerkElement* schaltwerkElement;
-  ListenElement* next;
-
-  SchaltwerkElement* getSchaltwerkElement();
-  ListenElement* getNextElement();
-/*Diese Methoden dienen zum Lesen der privaten Attribute eines einzelnen Objekts vom Typ
-ListenElement. Diese Methoden können auch inline implementiert werden*/
-  void setSchaltwerkElement( SchaltwerkElement* schaltwerkEl );
-  void setNextElement( ListenElement* nextEl );
-/*Diese Methoden dienen zum Schreiben der privaten Attribute eines einzelnen Objekts vom Typ
-ListenElement. Diese Methoden koennen auch inline implementiert werden.
-
-};
-*/
